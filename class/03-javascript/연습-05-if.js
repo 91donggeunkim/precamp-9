716d773f4911c2c1579178fc0f3ff014bad2a3c1
kim <91donggeunkim@gmail.com>
2021-10-29 18:22:17 +0900

    나의 첫번째 잔디

---
+++ class/03-javascript/연습-05-if.js
@@ -0,0 +1,44 @@
+if(1+1===2){
+    // console.log("정답입니다!!!")
+} else {
+    // console.log("틀렸습니다!!!")
+}
+// VM5111:2 정답입니다!!!
+// undefined
+if(true){
+    // console.log("정답입니다!!!")
+} else {
+    // console.log("틀렸습니다!!!")
+}    
+// VM5156:2 정답입니다!!!
+// undefined
+if(1+1===3){
+    // console.log("정답입니다!!!")
+} else {
+    // console.log("틀렸습니다!!!")
+}    
+// VM5181:4 틀렸습니다!!!
+// undefined
+let age=13
+// undefined
+age
+// 13
+if(age >= 20){
+    // console.log("성인입니다!!!")
+} else if(age >= 8) {
+    // console.log("학생입니다!!!")
+} else {
+    // console.log("어린이입니다!!!")
+}
+// VM5529:4 학생입니다!!!
+// undefined
+const profile = {
+    name: "철수",
+    age: 7,
+    school: "다람쥐초등학교"
+}
+// undefined
+profile.age
+// 7
+profile.name
+// '철수'
